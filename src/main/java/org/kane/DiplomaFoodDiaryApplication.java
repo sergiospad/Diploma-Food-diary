94a39d0c31c8b1c9ba1da14f91fd1cc2e36fca95
sergiospad <Lordgrey663@gmail.com>
2026-03-28 16:20:36 +0400

    The beginning is a very delicate time

---
+++ src/main/java/org/kane/DiplomaFoodDiaryApplication.java
@@ -0,0 +1,13 @@
+package org.kane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaFoodDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaFoodDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kane/DiplomaFoodDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaFoodDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
