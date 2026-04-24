6747d276f6fc34bb7eb284f92f5f65a65db169d5
sergiospad <Lordgrey663@gmail.com>
2026-04-24 18:20:04 +0400

    Добавлен CategoryService

---
+++ src/app/service/recipe_resource/ingredient.service.ts
@@ -0,0 +1,36 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Endpoint} from '../../util/endpoint';
+import {Observable} from 'rxjs';
+import IngredientShowDTO from '../../DTO/entity_dto/recipe-recource/ingredient/ingredient-show.dto';
+import IngredientChangeDTO from '../../DTO/entity_dto/recipe-recource/ingredient/ingredient-change.dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IngredientService {
+  private readonly http = inject(HttpClient);
+  private readonly ingredientAPI = new Endpoint('ingredient');
+
+  removeIngredient(id: number):Observable<any>{
+    return this.http.delete(
+      this.ingredientAPI.builder()
+        .points("delete", id.toString())
+        .build());
+  }
+
+  getShowIngredients(id:number):Observable<IngredientShowDTO[]>{
+    return this.http.get<IngredientShowDTO[]>(
+      this.ingredientAPI.builder()
+        .points("get", id.toString())
+        .build());
+  }
+
+  toggleMeasureUnit(ingredient:IngredientChangeDTO):Observable<IngredientShowDTO>{
+    return this.http.post<IngredientShowDTO>(
+      this.ingredientAPI.builder()
+        .points("toggle")
+        .build(),
+      ingredient)
+  }
+}
