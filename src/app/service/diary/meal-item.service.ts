f23fc50fcb07ff37e6c18ed38062f8f20d4485f5
sergiospad <Lordgrey663@gmail.com>
2026-04-25 00:36:27 +0400

    Добавлен MealItemService

---
+++ src/app/service/diary/meal-item.service.ts
@@ -0,0 +1,27 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Endpoint} from '../../util/endpoint';
+import MealItemEditDTO from '../../DTO/entity_dto/diary/meal_item/meal-item-edit.dto';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MealItemService {
+  private readonly http = inject(HttpClient);
+  private readonly mealItemAPI = new Endpoint('mealItem');
+
+  updateMealItem(mealItem: MealItemEditDTO):Observable<any>{
+    return this.http.patch(this.mealItemAPI.builder()
+        .points("edit")
+        .build(),
+      mealItem);
+  }
+
+  deleteMealItem(id:number):Observable<any>{
+    return this.http.delete(
+      this.mealItemAPI.builder()
+        .points("delete", id.toString())
+        .build());
+  }
+}
