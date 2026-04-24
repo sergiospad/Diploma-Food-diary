7f4ea36d54f085885e1444c2f088fd2571a4f491
sergiospad <Lordgrey663@gmail.com>
2026-04-25 01:00:34 +0400

    Добавлен SportsActivityService

---
+++ src/app/DTO/entity_dto/diary/sport_activity/sports-activity-create.dto.ts
@@ -0,0 +1,5 @@
+export default interface SportsActivityCreateDTO{
+  name:string;
+  calories: number;
+  diaryRecordDate:Date;
+}
---
+++ src/app/service/diary/sports-activity.service.ts
@@ -0,0 +1,38 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Endpoint} from '../../util/endpoint';
+import SportsActivityCreateDTO from '../../DTO/entity_dto/diary/sport_activity/sports-activity-create.dto';
+import {Observable} from 'rxjs';
+import SportActivityShowDTO from '../../DTO/entity_dto/diary/sport_activity/sport-activity-show.dto';
+import SportActivityEditDTO from '../../DTO/entity_dto/diary/sport_activity/sport-activity-edit.dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SportsActivityService {
+  private readonly http = inject(HttpClient);
+  private readonly activityAPI = new Endpoint('sportsActivity');
+
+  createSportActivity(activity: SportsActivityCreateDTO):Observable<SportActivityShowDTO>{
+    return this.http.put<SportActivityShowDTO>(
+      this.activityAPI.builder()
+        .points("create")
+        .build(),
+      activity)
+  }
+
+  editActivity(activity: SportActivityEditDTO):Observable<SportActivityShowDTO>{
+    return this.http.patch<SportActivityShowDTO>(
+      this.activityAPI.builder()
+        .points("edit")
+        .build(),
+      activity)
+  }
+
+  deleteActivity(id: number):Observable<any>{
+    return this.http.delete(
+      this.activityAPI.builder()
+        .points("delete", id.toString())
+        .build())
+  }
+}
